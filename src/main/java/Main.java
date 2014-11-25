0485017b50b094fd1e47ac9deaa309669f84b4e7
versteey <lolear@live.nl>
2014-11-25 16:23:18 +0100

    created main and main tests

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by eversteeg on 25-11-2014.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,7 @@
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase {
+    public void test_true(){
+        assertTrue(true);
+    }
+}
